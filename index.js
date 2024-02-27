82115217fa205a182a7a6e9a7e780c0c9753baff
M Danial <m.danial465@gmail.com>
2024-02-27 23:06:48 +0500

    Animation added

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+gsap.to(".image-container",{
+    ease: Expo.easeInOut,
+    width: "100%",
+    // duration: 2,
+    stagger: 2
+})
 
+gsap.to(".text h1",{
+    ease: Expo.easeInOut,
+    top: 0,
+    stagger: 2
+})
+
+gsap.to(".text h1",{
+    delay: 2,
+    ease: Expo.easeInOut,
+    top: "-100%",
+    stagger: 2
+})
